ccabdd8bc87301d66271d4b221fb8d47592f5add
raidelo <raidelosix@gmail.com>
2024-09-07 14:27:17 -0400

    añadiendo función para insertar los datos por la entrada estándar

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from string import ascii_letters, punctuation
 from requests import get, post
 
 class Video:
-    def __init__(self, id, key, size="??? MB"):
+    def __init__(self, id, key, size="??? MB", quality='none'):
         self.id = id
         self.key = key
         self.size = size
+        self.quality = quality
 
     def __str__(self) -> str:
-        return "Video {{ id: {id}, key: {key}, size: {size} }}".format(id=self.id,
-                                                                   key=self.key,
-                                                                   size=self.size,
-                                                                   )
+        return "Video {{ id: {id}, key: {key}, size: {size}, quality: {quality} }}".format(id=self.id,
+                                                                                   key=self.key,
+                                                                                   size=self.size,
+                                                                                   quality=self.quality,
+                                                                                   )
     def __repr__(self) -> str:
         return self.__str__()
 
@@ -65,15 +67,19 @@ class YTDownloader:
         info = self.__analyze(self.__target)
         return self.__save_info(info)
 
-    def download(self, calidad:str):
+    def download(self, calidad):
         try:
-            if not calidad.endswith('p'):
-                calidad += 'p'
+            if isinstance(calidad, int):
+                if not calidad in self.__data['qualities'].keys():
+                    print("error: calidad no disponible -> {}".format(calidad))
+                    exit(1)
+            elif isinstance(calidad, str):
+                calidad = int(calidad.strip('p'))
             vid = self.__data["qualities"][calidad]
-            self.__download(vid)
         except KeyError:
             print("error: calidad no disponible -> {}".format(calidad))
             exit(1)
+        self.__download(vid)
         self.reset_target()
 
     def __download(self, video):
@@ -100,7 +106,7 @@ class YTDownloader:
         downloaded = 0
         target_size = int(response.headers["Content-Length"].strip())
         with open(video_title+".mp4", "wb") as file:
-            print("Downloading video: {}.mp4".format(video_title))
+            print("Downloading video: {}.mp4 ({})".format(video_title, video.quality))
             for chunk in iterator:
                 file.write(chunk)
                 downloaded += len(chunk)
@@ -114,20 +120,24 @@ class YTDownloader:
             raise KeyError(self.__missing_data_error)
     
     def __save_info(self, info:dict):
-        strip_stuff = lambda x: x.strip((ascii_letters + punctuation + ' ').replace('p', ''))
-        if "mp4" in info["links"]:
+        strip_stuff = lambda x: x.strip((ascii_letters + punctuation + ' '))
+        if "links" not in info.keys():
+            print("error: no hay contenido para descargar")
+            return False
+        elif "mp4" in info["links"]:
             self.__data["title"] = info["title"]
             self.__data["vid"] = info["vid"]
             self.__data["qualities"] = {}
             for quality_data in info["links"]["mp4"].values():
                 if quality_data["q"] != "auto" and quality_data["f"] == "mp4":
-                    self.__data["qualities"][strip_stuff(quality_data["q"])] = Video(info["vid"],
-                                                                                     quality_data["k"],
-                                                                                     quality_data["size"] if quality_data["size"][0].isnumeric() else "??? MB",
-                                                                                     )
+                    self.__data["qualities"][int(strip_stuff(quality_data["q"]))] = Video(info["vid"],
+                                                                                          quality_data["k"],
+                                                                                          quality_data["size"] if quality_data["size"][0].isnumeric() else "??? MB",
+                                                                                          quality_data["q"],
+                                                                                          )
             return True
         else:
-            print(self.__missing_data_error)
+            print("error: no hay contenido para descargar")
             return False
 
     def __analyze(self, url:str) -> dict:
@@ -159,36 +169,68 @@ class YTDownloader:
             print(d)
         return d
 
-    def print_available_qualities(self):
+    def print_available_qualities(self, add_choices:bool=False):
         if not self.__check_data():
             raise KeyError(self.__missing_data_error)
-        t = self.__data["qualities"].items()
-        print("{} de video disponible para descargar:".format("Calidades" if len(t) > 1 else "Calidad"))
-        for qual, video in t:
-            print("{} -> {}".format(qual, video.size))
+        qualities = self.__data["qualities"]
+        list_of_qualities = [key for key in qualities.keys()]
+        list_of_qualities.sort(reverse=True)
+        print("{} de video disponible para descargar:".format("Calidades" if len(list_of_qualities) > 1 else "Calidad"))
+        for pos, key in enumerate(list_of_qualities):
+            print("{}{} -> {}".format("[{}] - ".format(pos+1) if add_choices else '', str(key)+'p', qualities[key].size))
+        return tuple(list_of_qualities)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("link",
+    parser.add_argument("link", nargs="?", default='',
                         help="el link del vídeo que quieres descargar")
-    parser.add_argument("calidad", nargs="?",
+    parser.add_argument("calidad", nargs="?", default='',
                         help="la calidad en la que prefieres descargar el vídeo")
+    parser.add_argument("--stdin", action="store_true",
+                        help="flag para ingresar tanto la url como la calidad por la entrada de datos estándar (stdin)")
     args = parser.parse_args()
 
     downloader = YTDownloader()
 
-    downloader.set_target(args.link)
+    if args.stdin:
+        if args.link:
+            parser.error("argument --stdin: not allowed with argument link or calidad")
+        link = input("Ingrese el link: ").strip()
+    elif args.link:
+        link = args.link
+    else:
+        parser.print_help()
+        exit(1)
 
-    downloader.get_info()
+    downloader.set_target(link)
+
+    if not downloader.get_info():
+        print("error: hubo un error al intentar obtener información del vídeo")
+        exit(1)
 
     print("Link de vídeo detectado! -> {}".format(downloader.get_video_name()))
 
-    if not args.calidad:
-        downloader.print_available_qualities()
-        exit(0)
+    if args.stdin:
+        ammount_qualities = downloader.print_available_qualities(True)
+        calidad = input("-> ").strip()
+        try:
+            number = int(calidad)
+        except ValueError:
+            print("Opción inválida")
+            exit(1)
+        if not (number > 0 and number <= len(ammount_qualities)):
+            print("Opción inválida")
+            exit(1)
+        calidad = ammount_qualities[number]
+    else:
+        if not args.calidad:
+            downloader.print_available_qualities()
+            exit(1)
+        else:
+            calidad = args.calidad
 
     try:
-        downloader.download(args.calidad)
+        downloader.download(calidad if args.stdin else args.calidad)
     except KeyboardInterrupt:
         pass
     finally:
